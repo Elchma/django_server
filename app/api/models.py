4d63a7c8b6ae03c479b9cb7c5d5716f4ee099bb3
Quentin <borhink@gmail.com>
2022-01-20 20:39:33 +0100

    Django-Restframework setup + Model view and serializer creation

--- app/api/models.py
+++ app/api/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class Coordinate(models.Model):
+    id = models.AutoField(primary_key=True)
+    x = models.FloatField()
+    y = models.FloatField()
+    z = models.FloatField()
+
+    def __str__(self):
+        return f'{self.x},{self.y},{self.z}'
