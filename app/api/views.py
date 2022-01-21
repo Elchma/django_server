f0cab1a3449f6a743b97875c6fcc7cbcfb24f62e
Quentin <borhink@gmail.com>
2022-01-21 01:29:16 +0100

    Create custom message when adding new coordinates with success

--- app/api/views.py
+++ app/api/views.py
@@ -1,4 +1,5 @@
-from rest_framework import viewsets
+from rest_framework import viewsets, status
+from rest_framework.response import Response
 
 from .models import Coordinate
 from .serializers import CoordinateSerializer
@@ -6,3 +7,13 @@ from .serializers import CoordinateSerializer
 class CoordinateViewSet(viewsets.ModelViewSet):
     queryset = Coordinate.objects.all()
     serializer_class = CoordinateSerializer
+
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        self.perform_create(serializer)
+        headers = self.get_success_headers(serializer.data)
+        return Response(
+             {"Success": "Coordinate added successfully"},
+             status=status.HTTP_201_CREATED,
+             headers=headers)
